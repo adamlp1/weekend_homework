369909f697b54ba152b28fdc0fbb5f610a440069
adam <adam.lewispetrie@gmail.com>
2019-10-18 15:57:37 +0100

    fixes stock count

--- pet_shop.rb
+++ pet_shop.rb
@@ -10,4 +10,19 @@ end
 
 def pets_sold(shop)
   return shop[:admin][:pets_sold]
+end
+
+def increase_pets_sold(shop, quantity)
+  shop[:admin][:pets_sold] += quantity
+end
+
+# def stock_count(shop)
+#   total_stock = 0
+#   for animals in shop
+#     total_stock += 1
+#   end
+#     return total_stock
+#   end
+def stock_count(shop)
+  total_stock = shop[:pets].length
 end 
